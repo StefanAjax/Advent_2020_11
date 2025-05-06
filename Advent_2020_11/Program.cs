5cbd95b10f32785a86d41b33e0836b5c95e8f36d
StefanAjax <pianoajax@gmail.com>
2025-05-06 09:10:32 +0200

    Add project files.

---
+++ Advent_2020_11/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Advent_2020_11
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
